4c41749df395c67bf734f54d3f8edfbda601c8e1
FerreiraAlvesP <pferreira.etudes@gmail.com>
2021-02-12 12:24:56 +0100

    Ajout menu vide

---
+++ Assets/Scripts/menuAnimScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuAnimScript : MonoBehaviour
+{
+    public GameObject MenuPanel;
+
+    public void MenuAnimation()
+    {
+        if(MenuPanel != null)
+        {
+            Animator animator = MenuPanel.GetComponent<Animator>();
+            if(animator != null)
+            {
+                bool showMenu = animator.GetBool("showMenu");
+                animator.SetBool("showMenu", !showMenu);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

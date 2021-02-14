bb14bb11e81de9c7e76f4945e684e4b9611864f8
FerreiraAlvesP <pferreira.etudes@gmail.com>
2021-02-14 19:36:45 +0100

    "transparency"

---
+++ Assets/Scripts/TransparencyScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparencyScript : MonoBehaviour
+{
+    //public Slider transparencySlider;
+    public GameObject brush;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void test()
+    {
+        Renderer renderer;
+        renderer = brush.GetComponent<Renderer>();
+
+        if (renderer != null)
+        {
+            //renderer.material.color.a = val;
+            Color c = renderer.sharedMaterial.color;
+            c.a = 0;
+
+            renderer.sharedMaterial.SetColor("_Color", c);
+        }
+    }
+
+    public void changeTransparency(float val)
+    {
+        Renderer renderer;
+        renderer = brush.GetComponent<Renderer>();
+
+        if (renderer != null)
+        {
+            //renderer.material.color.a = val;
+            Color c = renderer.sharedMaterial.color;
+            c.a = val;
+
+            renderer.sharedMaterial.SetColor("_Color", c);
+        }
+        //float t = transparencySlider.value;
+    }
+}

65204ea78f2ea1d2aa6ee4bb6617d31a0b57e3c1
NicolasFlz <78754555+NicolasFlz@users.noreply.github.com>
2021-02-14 23:08:29 +0100

    ARPainting v1

---
+++ Assets/Scripts/ARWriting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARWriting : MonoBehaviour {
+    public GameObject paint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            //instanciate a brush
+            Instantiate(paint, transform.position, transform.rotation);
+        }
+    }
+}
--- Assets/Scripts/Paint.cs
+++ Assets/Scripts/Paint.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Paint : MonoBehaviour
-{
+public class Paint : MonoBehaviour {
+    public GameObject target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("ARCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(target.transform);
     }
 }

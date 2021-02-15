bf8aa9f49254f17253b4f58d2ea81499a7b20806
Johann Equilbec <johann.equilbec@universite-paris-saclay.fr>
2021-02-15 16:48:30 +0100

    Fix drawing on board

--- Assets/Scripts/Paintable.cs
+++ Assets/Scripts/Paintable.cs
@@ -19,16 +19,16 @@ public class Paintable : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0)) // Equivalent to touch the screen
         {
-            //cast a ray to the plane
+            // Cast a ray
             var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(Ray, out hit))
             {
-                //instanciate a brush
-                var go = Instantiate(Brush, hit.point + Vector3.up * 0.1f, Quaternion.identity, transform);
-                go.transform.localScale = Vector3.one * BrushSize;
+                // Instanciate a brush
+                var go = Instantiate(Brush, hit.point + Vector3.up * 0.0015f, Quaternion.identity, transform); // Vector3.up * 0.1f
+                go.transform.localScale = Vector3.one * BrushSize; 
             }
 
         }
@@ -41,19 +41,19 @@ public class Paintable : MonoBehaviour
 
     private IEnumerator CoSave()
     {
-        //wait for rendering
+        // Wait for rendering
         yield return new WaitForEndOfFrame();
         Debug.Log(Application.dataPath + "/savedImage.png");
 
-        //set active texture
+        // Set active texture
         RenderTexture.active = RTexture;
 
-        //convert rendering texture to texture2D
+        // Convert rendering texture to texture2D
         var texture2D = new Texture2D(RTexture.width, RTexture.height);
         texture2D.ReadPixels(new Rect(0, 0, RTexture.width, RTexture.height), 0, 0);
         texture2D.Apply();
 
-        //write data to file
+        // Write data to file
         var data = texture2D.EncodeToPNG();
         File.WriteAllBytes(Application.dataPath + "/savedImage.png", data);
 
